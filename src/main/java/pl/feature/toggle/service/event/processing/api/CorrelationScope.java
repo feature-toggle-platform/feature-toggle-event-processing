6272b293bf5352384bc74afdd48f99ecae60fa54
SaQie <Kamcio99@interia.com>
2026-03-17 20:14:42 +0100

    added Correlation scope wrapper

---
+++ src/main/java/pl/feature/toggle/service/event/processing/api/CorrelationScope.java
@@ -0,0 +1,19 @@
+package pl.feature.toggle.service.event.processing.api;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.slf4j.MDC;
+import pl.feature.toggle.service.model.security.correlation.CorrelationId;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CorrelationScope {
+
+    public static void run(CorrelationId correlationId, Runnable runnable) {
+        try {
+            MDC.put(CorrelationId.MDCName(), correlationId.value());
+            runnable.run();
+        } finally {
+            MDC.remove(CorrelationId.MDCName());
+        }
+    }
+}
